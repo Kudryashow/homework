cfbcab04592e68ee07fa44e4fd0196a143e24935
Kudryashov <Jahma123@icloud.com>
2018-12-04 18:47:27 +0300

    es-done

--- app/index.php
+++ app/index.php
@@ -13,109 +13,231 @@
   <link rel="stylesheet" href="/css/main.min.css" type="text/css">
   <title>My project</title>
 </head>
+
 <body>
-   <div class="header">
-   <nav class="navbar">
-    <ul class="nav">
+  <div class="header">
+    <nav class="navbar">
+      <ul class="nav">
         <li class="nav-item">
-          <a href="#" class="nav-link navlink">
+          <a href="#" class="nav-link text-light navlink">
             <span style="color:white">LO</span><span style="color:red">GO</span>
           </a>
         </li>
-        <li class="nav-item"><a href="#" class="nav-link navlink">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link navlink">About</a></li>
-        <li class="nav-item active"><a href="#" class="nav-link navlink">Services</a></li>
-        <li class="nav-item"><a href="#" class="nav-link navlink">Portfolio</a></li>
-        <li class="nav-item"><a href="#" class="nav-link navlink">Shop</a></li>
-        <li class="nav-item"><a href="#" class="nav-link navlink">Contact</a></li>
-        <li class="nav-item"><a href="#" class="nav-link navlink">Pages</a></li>
-        <li class="nav-item"><a href="#" class="nav-link navlink">Social</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-light navlink">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-light navlink">About</a></li>
+        <li class="nav-item active"><a href="#" class="nav-link text-light navlink">Services</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-light navlink">Portfolio</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-light navlink">Shop</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-light navlink">Contact</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-light navlink">Pages</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-light navlink">Social</a></li>
       </ul>
     </nav>
 
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner align-items-center">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner carousel-inner-header">
         <div class="carousel-item active">
           <div class="inner-slide m-5 p-2 myborder text-white text-center align-middle">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore necessitatibus, sunt quidem fugiat vero veritatis.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore necessitatibus, sunt quidem fugiat vero
+            veritatis.
           </div>
         </div>
         <div class="carousel-item">
           <div class="inner-slide m-5 p-2 myborder align-middle">
             <h2 class="font-weight-bold text-uppercase text-center text-white">The best services</h2>
-            <p class="text-white">We provide the best services in the world. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore ipsa incidunt deserunt at beatae.</p>
+            <p class="text-white">We provide the best services in the world. Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Labore ipsa incidunt deserunt at beatae.</p>
             <button type="button" class="btn btn-light out-border">Clickme</button>
           </div>
         </div>
       </div>
       <a class="carousel-control-prev carousel-control" href="#carouselExampleControls" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
       </a>
       <a class="carousel-control-next carousel-control" href="#carouselExampleControls" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
       </a>
+    </div>
+
   </div>
 
-</div>
-
-<div>
-  <div class="row">
-    <div class="col-sm book image"></div>
-    <div class="col-sm book pl-5 pt-4 pb-1">
-      <h2>Lorem ipsum dolor sit.</h2>
-      <p class="text-left font-weight-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur deserunt dignissimos enim unde iste minus, debitis atque harum. Labore deleniti velit saepe perspiciatis ut natus. Alias vero nulla quidem earum nesciunt facilis, ex ad, harum reiciendis quisquam aliquid in iste.</p>
-      <p class="text-left font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio mollitia dolor odio perspiciatis! Corporis, neque tempore repellendus iusto consectetur illum adipisci eos sed. Adipisci libero optio odio facere quia voluptatibus quos esse consectetur, nulla totam corrupti, porro aut explicabo doloremque.</p>
-      <button class="btn btn-primary">Read more details</button>
+  <div class="split-2">
+    <div class="row">
+      <div class="col-sm book image"></div>
+      <div class="col-sm book pl-5 pt-4 pb-1">
+        <h2>Lorem ipsum dolor sit.</h2>
+        <p class="text-left font-weight-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          deserunt dignissimos enim unde iste minus, debitis atque harum. Labore deleniti velit saepe perspiciatis ut
+          natus. Alias vero nulla quidem earum nesciunt facilis, ex ad, harum reiciendis quisquam aliquid in iste.</p>
+        <p class="text-left font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+          mollitia dolor odio perspiciatis! Corporis, neque tempore repellendus iusto consectetur illum adipisci eos
+          sed. Adipisci libero optio odio facere quia voluptatibus quos esse consectetur, nulla totam corrupti, porro
+          aut explicabo doloremque.</p>
+        <button class="btn btn-primary">Read more details</button>
+      </div>
     </div>
   </div>
-</div>
 
-<div class="block">
-  <div class="services crosslayer"></div>
-  <div class="row">
-    <div class="col"></div>
-    <div class="col"></div>
-    <div class="col"></div>
+  <div class="block">
+    <div class="services crosslayer"></div>
+    <div class="row">
+      <div class="col puzzles">
+        <img src="/img/notebook.png" alt="preview" class="img-fluid note-pic">
+        <h3 class="text-uppercase">Web-development</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae laborum doloribus iusto sequi amet commodi.</p>
+        <button class="btn central-btn">More details</button>
+      </div>
+      <div class="col puzzles">
+        <img src="/img/notebook.png" alt="preview" class="img-fluid note-pic">
+        <h3 class="text-uppercase">Web-development</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae laborum doloribus iusto sequi amet commodi.</p>
+        <button class="btn central-btn">More details</button>
+      </div>
+      <div class="col puzzles">
+        <img src="/img/notebook.png" alt="preview" class="img-fluid note-pic">
+        <h3 class="text-uppercase">Web-development</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae laborum doloribus iusto sequi amet commodi.</p>
+        <button class="btn central-btn">More details</button>
+      </div>
+    </div>
+    <div class="plus-size"></div>
   </div>
-</div>
 
-<div class="block">
-  <div class="feedback crosslayer"></div>
-  <div class="carousel slide">
-    <div class="carousel-inner carousel-item">enim velit corrupti</div>
-    <div class="carousel-inner carousel-item">eum et eius</div>
-    <div class="carousel-inner carousel-item">blanditiis sequi excepturi</div>
-    <div class="carousel-inner carousel-item">commodi ipsam ut</div>
-    <div class="carousel-inner carousel-item">saepe libero voluptatum</div>
-    <div class="carousel-inner carousel-item">dolores ut consequatur</div>
-    <div class="carousel-inner carousel-item">necessitatibus exercitationem fugiat</div>
-    <div class="carousel-inner carousel-item">quod autem est</div>
-    <div class="carousel-inner carousel-item">quibusdam dolor accusantium</div>
-    <div class="carousel-inner carousel-item">nesciunt magnam non</div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+  <div class="block">
+    <div class="feedback crosslayer"></div>
+    <div class="row mt-5">
+      <div class="col-lg-5 p-5">
+
+
+        <div id="side-slide" class="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">If we navigate the driver, we can get to the HDD capacitor through the
+              back-end EXE matrix! <br> <span>________</span> </div>
+            <div class="carousel-item">I'll compress the bluetooth FTP program, that should hard drive the EXE
+              microchip! <br> <span>________</span> </div>
+            <div class="carousel-item">We need to reboot the open-source EXE hard drive!<br> <span>________</span>
+            </div>
+          </div>
+          <a id="arr-1" class="carousel-control-prev carousel-control" href="#side-slide" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <span id="arr-1 text-center" class="carousel-control">1/10</span>
+          <a id="arr-1" class="carousel-control-next carousel-control" href="#side-slide" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+          <div class="plus-size"></div>
+        </div>
+
+
+
+      </div>
+      <div class="col-lg-2">
+        <div class="bgu img-1">
+          <div class="card-comment card-info">Imran Khan <br> CEO, Vesso</div>
+          <div class="card-number card-info">01</div>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <div class="bgu img-2">
+          <div class="card-comment card-info">Imran Khan <br> CEO, Vesso</div>
+          <div class="card-number card-info">02</div>
+        </div>
+      </div>
+      <div class="col-lg-2">
+        <div class="bgu img-3">
+          <div class="card-comment card-info">Imran Khan <br> CEO, Vesso</div>
+          <div class="card-number card-info">03</div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
 
-<div class="block">
-  <div class="clients crosslayer"></div>
-  <div class="row">
-    <div class="col"></div>
-    <div class="col"></div>
-    <div class="col"></div>
+  <div class="block-white">
+    <div class="crosslayer clients"></div>
+    <div class="raw client-list text-center">
+      <img src="/img/staff.png" alt="" class="col-sm-4">
+      <img src="/img/staff.png" alt="" class="col-sm-4">
+      <img src="/img/staff.png" alt="" class="col-sm-4">
+      <img src="/img/staff.png" alt="" class="col-sm-4">
+      <img src="/img/staff.png" alt="" class="col-sm-4">
+      <img src="/img/staff.png" alt="" class="col-sm-4">
+    </div>
   </div>
-</div>
+  <div class="crosslayer quote">
+    <div class="col-md-6" id="top-quote">
+      <h2>Any Queries | Get a quote</h2>
+      <p>Quas expedita quibusdam autem facere explicabo in sed nesciunt. <br>
+        Blanditiis aspernatur aut accusantium.</p>
+    </div>
+    <div class="col-md-6" id="quote-form">
+      <form class="form-inline">
+        <div class="form-group custom">
+          <input type="text" placeholder="You name" id="but-1" class="form-control" id="name" placeholder="You name">
+          <input type="text" placeholder="You number" id="but-2" class="form-control" id="number" placeholder="You number">
+          <button type="submit" id="but-3" class="btn" id="submit">Get a call</button>
+        </div>
+      </form>
+    </div>
 
-<div class="call"></div>
-<div class="footer"></div>
+  </div>
+
+  <div class="footer">
+    <div class="row p-3">
+      <div class="col-sm-4">
+        <nav class="nav flex-column">
+          <a href="#" class="bigger font-weight-bold text-light ">PRODUCTS</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+        </nav>
+      </div>
+      <div class="col-sm-4">
+        <nav class="nav flex-column">
+          <a href="#" class="bigger font-weight-bold text-light ">LEARN</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+        </nav>
+      </div>
+      <div class="col-sm-4">
+        <nav class="nav flex-column">
+          <a href="#" class="bigger font-weight-bold text-light ">COMPANY</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+          <a class="nav-link text-light" href="#">Link</a>
+        </nav>
+      </div>
+      <div class="col-sm-4">
+        <div id="quote-form">
+          <form class="form-inline">
+            <div class="form-group custom">
+              <input type="text" placeholder="You email" id="but-1" class="form-control subscribe" id="name" placeholder="You name">
+              <button type="submit" id="but-3" class="btn subscribe">Subscribe</button>
+            </div>
+          </form>
+        </div>
+        <div class='col-sm-4 social'>
+          <ul class="nav">
+            <li class="nav-item"><a href="#" class="facebook"><img src="/img/soc.png" alt="facebook"></a></li>
+            <li class="nav-item"><a href="#" class="instagram"><img src="/img/soc.png" alt="instagram"></a></li>
+            <li class="nav-item"><a href="#" class="gmail"><img src="/img/soc.png" alt="gmail"></a></li>
+            <li class="nav-item"><a href="#" class="vk"><img src="/img/soc.png" alt="vk"></a></li>
+          </ul>
+          
+          
+          
+          
+        </div>
+      </div>
+    </div>
+  </div>
 
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
